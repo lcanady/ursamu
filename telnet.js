04020c62d37e202d15d0c369a4df0665a68456f8
Lemuel Canady <lem.canady@gmail.com>
2021-10-13 11:00:37 -0700

    update reconnect conditions

--- telnet.js
+++ telnet.js
@@ -1,7 +1,7 @@
 const telnetlib = require("telnetlib");
 const io = require("socket.io-client");
 
-const connect = `
+const intro = `
 
  ...    ::::::::::..   .::::::.   :::.     .        :    ...    :::
  ;;     ;;;;;;;'';;;; ;;;'    '   ;;';;    ;;,.    ;;;   ;;     ;;;
@@ -24,8 +24,7 @@ const server = telnetlib.createServer(
   },
   async (c) => {
     let token;
-    const s = io("http://localhost:1337");
-    c.write(connect + "\r\n");
+    c.write(intro + "\r\n");
 
     const naws = c.getOption(NAWS);
 
@@ -35,34 +34,47 @@ const server = telnetlib.createServer(
       c.height = data.height;
     });
 
-    s.on("message", (ctx) => {
-      if (ctx?.data?.token) token = ctx?.data?.token;
-      if (ctx.msg) c.write(ctx.msg + "\r\n");
-    });
-
-    s.io.on("reconnect", () => {
-      s.send({
-        data: { token, height: c.height, width: c.width },
-        msg: "think ...Reconnected...",
+    const connect = () => {
+      const s = io("http://localhost:1337");
+      s.on("message", (ctx) => {
+        if (ctx?.data?.token) token = ctx?.data?.token;
+        if (ctx.msg) c.write(ctx.msg + "\r\n");
       });
-    });
 
-    s.on("quit", () => c.end());
-
-    s.on("error", (err) => {
-      console.error(err);
-    });
-
-    c.on("end", () => s.close());
-
-    c.on("error", (err) => console.log(err));
-
-    c.on("data", (data) => {
-      s.send({
-        data: { token, height: c.height, width: c.width },
-        msg: data.toString(),
+      s.io.on("reconnect", () => {
+        s.send({
+          data: { token, height: c.height, width: c.width },
+          msg: "think ...Reconnected...",
+        });
       });
-    });
+
+      s.on("disconnect", (reason) => {
+        if (reason.includes("close") || reason.includes("timeout")) {
+          connect();
+        } else {
+          c.end();
+        }
+      });
+
+      s.on("quit", () => c.end());
+
+      s.on("error", (err) => {
+        console.error(err);
+      });
+
+      c.on("end", () => s.close());
+
+      c.on("error", (err) => console.log(err));
+
+      c.on("data", (data) => {
+        s.send({
+          data: { token, height: c.height, width: c.width },
+          msg: data.toString(),
+        });
+      });
+    };
+
+    connect();
   }
 );
 
